53396ef7a5b92e269bdf82fc448b2a5cc6317143
bSuslu <51022680+bSuslu@users.noreply.github.com>
2025-11-25 15:17:16 +0300

    added persistent bootstrapper

---
+++ Assets/_Project/Gameplay/Common/ServiceLocatorGlobalPersistentBootstrapper.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using _Project.Core.Framework.ServiceLocator;
+using _Project.Core.Framework.ServiceLocator.Bootstrappers;
+using _Project.Core.Systems.CurrencySystem.Interfaces;
+using _Project.Core.Systems.CurrencySystem.Scripts;
+using _Project.Core.Systems.CurrencySystem.Services;
+using _Project.Core.Systems.LoadingSystem.Interfaces;
+using _Project.Core.Systems.SaveSystem.Interfaces;
+using _Project.Core.Systems.SaveSystem.Services;
+using _Project.Core.Systems.SceneSystem;
+using Cysharp.Threading.Tasks;
+
+namespace TMC._Project.Gameplay.Common
+{
+    public class ServiceLocatorGlobalPersistentBootstrapper: ServiceLocatorGlobalBootstrapper
+    {
+        private readonly List<IAsyncService> _asyncServices = new();
+        protected override void Bootstrap()
+        {
+            base.Bootstrap();
+
+            var save = new JsonSaveService();
+            ServiceLocator.Global.Register<ISaveService>(save);
+            RegisterIfAsync(save);
+
+            var currency = new CurrencyService();
+            ServiceLocator.Global.Register<ICurrencyService>(currency);
+            RegisterIfAsync(currency);
+
+            var scene = new SceneService();
+            ServiceLocator.Global.Register(scene);
+            RegisterIfAsync(scene);
+        }
+
+        private void RegisterIfAsync(object service)
+        {
+            if (service is IAsyncService asyncService)
+                _asyncServices.Add(asyncService);
+        }
+        
+        // private async void Start()
+        // {
+        //     foreach (var s in _asyncServices)
+        //         await s.InitializeAsync();
+        //
+        //     // loading tamamlandı
+        // }
+        
+        private async void Start()
+        {
+            var tasks = new List<UniTask>();
+
+            foreach (var service in _asyncServices)
+                tasks.Add(service.InitializeAsync());
+
+            await UniTask.WhenAll(tasks);
+        }
+    }
+}
