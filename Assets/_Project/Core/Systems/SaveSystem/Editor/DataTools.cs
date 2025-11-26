358ffc61318ae2a13bed92fdad44930b66b867da
bSuslu <51022680+bSuslu@users.noreply.github.com>
2025-11-26 14:45:17 +0300

    added data delete tool to easy test save system

---
+++ Assets/_Project/Core/Systems/SaveSystem/Editor/DataTools.cs
@@ -0,0 +1,44 @@
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace _Project.Core.Systems.SaveSystem.Editor
+{
+    public static class DataTools
+    {
+        [MenuItem("Data/Delete All Save Data")]
+        public static void DeleteAllPersistentData()
+        {
+            string path = Application.persistentDataPath;
+
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.Delete(path, true); 
+                    Debug.Log($"All data under {path} has been deleted.");
+                }
+                catch (IOException e)
+                {
+                    Debug.LogError($"Failed to delete save data: {e.Message}");
+                }
+            }
+            else
+            {
+                Debug.Log("No save data found.");
+            }
+            
+            // Clear all Player Prefs
+            PlayerPrefs.DeleteAll();
+            Debug.Log("All Player Prefs have been deleted.");
+        }
+        
+        [MenuItem("Data/Open Save Folder")]
+        public static void OpenPersistentDataPath()
+        {
+            EditorUtility.RevealInFinder(Application.persistentDataPath);
+        }
+    }
+}
+#endif
