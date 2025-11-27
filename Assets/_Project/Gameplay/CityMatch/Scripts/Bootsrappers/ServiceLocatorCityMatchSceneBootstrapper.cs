4dc395dc418e73a95e5ddb48d8dcc8bd286ce742
bSuslu <51022680+bSuslu@users.noreply.github.com>
2025-11-27 22:27:49 +0300

    added citymatch bootstrapper

--- Assets/_Project/Gameplay/CityMatch/Scripts/Bootsrappers/ServiceLocatorCityMatchSceneBootstrapper.cs
+++ Assets/_Project/Gameplay/CityMatch/Scripts/Bootsrappers/ServiceLocatorCityMatchSceneBootstrapper.cs
@@ -1,15 +1,24 @@
+using _Project.Core.Framework.EventBus;
+using _Project.Core.Framework.ServiceLocator;
 using _Project.Core.Framework.ServiceLocator.Bootstrappers;
+using _Project.Core.Systems.LoadingSystem.Events;
+using _Project.Core.Systems.TimeSystem.Interfaces;
+using _Project.Core.Systems.TimeSystem.Services;
 
-namespace TMC._Project.Gameplay.CityMatch.Scripts
+namespace TMC._Project.Gameplay.CityMatch.Scripts.Bootsrappers
 {
     public class ServiceLocatorCityMatchSceneBootstrapper: ServiceLocatorSceneBootstrapper
     {
         protected override void Bootstrap()
         {
             base.Bootstrap();
+
+            float time = 40f;
+            var gameTimerService = new GameTimerService();
+            ServiceLocator.ForSceneOf(this).Register<IGameTimerService>(gameTimerService);
+            gameTimerService.StartTimerAsync(time);
             
-            
-               
+            EventBus<ServicesReadyEvent>.Publish(new ServicesReadyEvent());
         }
     }
 }
