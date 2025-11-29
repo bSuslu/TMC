35b26b99f2e35fd80a65349b6d719cfbd9ac8e58
bSuslu <51022680+bSuslu@users.noreply.github.com>
2025-11-29 19:29:23 +0300

    Reapply "updated level service"
    This reverts commit 6cfb07854dfd7d89cba57e2fca1d0d784a3b383e.

--- Assets/_Project/Gameplay/Common/Scripts/LivesSystem/Settings/LivesSettings.cs
+++ Assets/_Project/Gameplay/Common/Scripts/LivesSystem/Settings/LivesSettings.cs
@@ -6,7 +6,8 @@ namespace TMC._Project.Gameplay.Common.Scripts.LivesSystem.Settings
     public class LivesSettings : ScriptableObject
     {
         [field: SerializeField] public int StartLives { get; private set; }
-        [field: SerializeField] public float RegenTime { get; private set; }
         [field: SerializeField] public int MaxLives { get; private set; }
+        
+        [field: SerializeField] public float RegenTime { get; private set; }
     }
 }
